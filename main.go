df8539baf5169c7bda1ad96463393f859491876d
Didier Roche <didier.roche@canonical.com>
2025-06-05 14:17:39 +0200

    Hook up basic command line parsing
    First stance of command line handling and autoselecting epics or tasks.

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"strings"
+
+	_ "embed"
+
+	"github.com/canonical/pulse-summarizer/internal/jira"
+	"github.com/k0kubun/pp"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func newViperConfig(name string) (*viper.Viper, error) {
+	vip := viper.New()
+	vip.SetEnvPrefix(strings.ReplaceAll(name, "-", "_"))
+	vip.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) // So Jira.Username → PULSE_SUMMARIZER_JIRA_USERNAME
+	vip.AutomaticEnv()
+	vip.SetConfigName(name)
+	vip.AddConfigPath(".")
+
+	if err := vip.ReadInConfig(); err != nil {
+		var e viper.ConfigFileNotFoundError
+		if errors.As(err, &e) {
+			slog.Info("No configuration file. We will only use the defaults, env variables or flags.")
+			return vip, nil
+		}
+		return nil, fmt.Errorf("invalid configuration file: %v", err)
+	}
+
+	return vip, nil
+}
+
+//go:embed pulse-summarizer.example.yaml
+var configExample string
+
+func main() {
+	name := "pulse-summarizer"
+	var config struct {
+		Jira struct {
+			Username string
+			APIToken string `mapstructure:"api_token"`
+		}
+	}
+
+	vip, err := newViperConfig(name)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading configuration: %v\n", err)
+		os.Exit(2)
+	}
+
+	rootCmd := cobra.Command{
+		Use:   fmt.Sprintf("%s [JIRA_TICKET…]", name),
+		Short: fmt.Sprintf("%s posts update frequently", name),
+		Long:  "Summarize the high level tickets based on recent activity on its children. If no Jira ticket is provided, all active assigned epics are considered.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			jiraClient, err := jira.NewClient("https://warthogs.atlassian.net", vip.GetString("jira.username"), vip.GetString("jira.api_token"))
+			if err != nil {
+				return fmt.Errorf("invalid jira Client: %v", err)
+			}
+
+			return summarize(jiraClient, args...)
+		},
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := vip.Unmarshal(&config); err != nil {
+				fmt.Fprintf(os.Stderr, "unable to decode configuration: %v", err)
+				os.Exit(1)
+			}
+
+			if vip.Get("jira.username") == nil || vip.Get("jira.api_token") == nil {
+				slog.Error(fmt.Sprintf(`ERROR: missing configuration. Please set:
+  * PULSE_SUMMARIZER_JIRA_USERNAME (your email)")
+  * PULSE_SUMMARIZER_IRA_API_TOKEN (API token from your Atlassian account)")
+You can also store them permanently in a configuration file named %s.yaml with:
+
+%v`, name, configExample))
+				os.Exit(2)
+			}
+
+			return nil
+
+		},
+	}
+
+	rootCmd.Flags().String("jira-username", "", "jira username to use to connect to")
+	err = vip.BindPFlag("jira.username", rootCmd.Flags().Lookup("jira-username"))
+	if err != nil {
+		log.Fatalf("programmer error: unable to bind flag jira-username: %v", err)
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func summarize(jc *jira.Client, topIssueKeys ...string) error {
+	var topIssues []jira.Issue
+	var err error
+
+	if len(topIssueKeys) == 0 {
+		if topIssues, err = jc.GetMyAssignedEpics(); err != nil {
+			return fmt.Errorf("error fetching epics: %v", err)
+		}
+	} else {
+		topIssues, err = jc.GetIssuesByKeys(topIssueKeys...)
+	}
+	if err != nil {
+		return fmt.Errorf("error fetching issues: %v", err)
+	}
+
+	pp.Println(topIssues)
+
+	return nil
+}
